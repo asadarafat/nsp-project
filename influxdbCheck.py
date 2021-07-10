009bb827dc9697947362060bc8ad6079e78dc1dc
asad <aarafat.me@gmail.com>
2021-07-10 13:00:04 +0200

    a

--- influxdbCheck.py
+++ influxdbCheck.py
@@ -1,21 +1,19 @@
-
 import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
 
-global ifluxdbIP
-global influxPort
-global influxUsername
-global influxPassword
+import socket
 
 
-ifluxdbIP       = '10.58.23.111'
-influxPort      = 8086
-influxUsername  = 'telemetry'
-influxPassword  = 'telemetry'
+host = socket.gethostname() 
+print(host)
 
+bucket = "nsp-bucket"
+org="nsp-org"
+token="Rt_B7M0P40LXvqs1OgrAeng5yxWUXvI2m9q3FhobdzEwi6W0Vf9VaK2MLON9l5RsrqfKPQHDQ6xBfPozy1TW_Q=="
 
-client = InfluxDBClient(host=ifluxdbIP, port=influxPort, username=influxUsername, password=influxPassword)
-print (client)
+client = influxdb_client.InfluxDBClient(url="http://10.58.23.122':8086", token=token, org=org)
 
+write_api = client.write_api(write_options=SYNCHRONOUS)
 
-
-
+p = influxdb_client.Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
+write_api.write(bucket=bucket, org=org, record=p)
